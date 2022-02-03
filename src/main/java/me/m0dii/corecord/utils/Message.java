7b1e31784454938f63bebd460a06e3f268b58185
M0diis <modestas.kazlauskas@ktu.edu>
2022-02-03 14:43:54 +0200

    PlaceholderAPI support & changeable messages.

---
+++ src/main/java/me/m0dii/corecord/utils/Message.java
@@ -0,0 +1,11 @@
+package me.m0dii.corecord.utils;
+
+public enum Message
+{
+    EMBED_RESULT_COUNT,
+    EMBED_NO_RESULTS,
+    EMBED_NO_RESULTS_FILTER,
+    EMBED_TITLE,
+    GAME_CONFIG_RELOAD,
+    COORDINATE_ROW
+}
